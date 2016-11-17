06b793aaada13be8aee21dbc29e0850a6517ab83
Segun Famisa <segunfamisa@gmail.com>
2016-11-17 04:22:28 +0100

    chore: added PinPadButton and PinPadView
    - implemented logic to retrieve and set attributes via xml and java code

---
+++ pinpad/src/main/java/co/paystack/android/design/widget/PinPadButton.java
@@ -0,0 +1,230 @@
+package co.paystack.android.design.widget;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+class PinPadButton extends ForegroundLinearLayout implements View.OnClickListener {
+    private static final float DEFAULT_TEXT_SIZE_NUMERIC = 18f;
+    private static final float DEFAULT_TEXT_SIZE_ALPHA = 12f;
+    private static final int DEFAULT_DRAWABLE_SIZE = 15;
+    @ColorInt
+    private static final int DEFAULT_TEXT_COLOR = Color.WHITE;
+
+    private float mTextSizeNumeric;
+    private float mTextSizeAlpha;
+    private int mDrawableSize;
+    private Drawable mButtonDrawable;
+    private String mTextNumeric;
+    private String mTextAlphabet;
+
+    private TextView mTextViewNumeric;
+    private TextView mTextViewAlphabet;
+    private ImageView mImageIcon;
+
+    private ColorStateList mTextColor = ColorStateList.valueOf(DEFAULT_TEXT_COLOR);
+    private OnButtonClickListener mButtonClickListener;
+
+    public interface OnButtonClickListener {
+        void onButtonClick(PinPadButton button);
+    }
+
+    public PinPadButton(Context context) {
+        super(context);
+    }
+
+    public PinPadButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public PinPadButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs, defStyle);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyle) {
+        if (context != null && attrs != null) {
+            View view = inflate(context, R.layout.layout_button, this);
+            mTextViewNumeric = (TextView) view.findViewById(R.id.numeric_text);
+            mTextViewAlphabet = (TextView) view.findViewById(R.id.alphabet_text);
+            mImageIcon = (ImageView) view.findViewById(R.id.pinbutton_icon);
+
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.PinPadView ,defStyle, 0);
+
+            mTextSizeNumeric = a.getDimension(R.styleable.PinPadView_button_numeric_textsize,
+                    DEFAULT_TEXT_SIZE_NUMERIC);
+            mTextSizeAlpha = a.getDimension(R.styleable.PinPadView_button_alpha_textsize,
+                    DEFAULT_TEXT_SIZE_ALPHA);
+            mDrawableSize = a.getDimensionPixelSize(R.styleable.PinPadView_button_drawable_size,
+                    15);
+            if (a.hasValue(R.styleable.PinPadView_button_drawable)) {
+                mButtonDrawable = a.getDrawable(R.styleable.PinPadView_button_drawable);
+            }
+            if (a.hasValue(R.styleable.PinPadView_button_text_numeric)) {
+                mTextNumeric = a.getString(R.styleable.PinPadView_button_text_numeric);
+            }
+            if (a.hasValue(R.styleable.PinPadView_button_text_alpha)) {
+                mTextAlphabet = a.getString(R.styleable.PinPadView_button_text_alpha);
+            }
+            if (a.hasValue(R.styleable.PinPadView_text_color)) {
+                mTextColor = a.getColorStateList(R.styleable.PinPadView_text_color);
+            }
+
+            if (mButtonDrawable == null) {
+                if (mTextNumeric != null && !mTextNumeric.isEmpty()) {
+                    // create numeric textview
+                    mImageIcon.setVisibility(GONE);
+                    mTextViewNumeric.setVisibility(VISIBLE);
+                    mTextViewNumeric.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTextSizeNumeric);
+                    mTextViewNumeric.setText(mTextNumeric);
+
+                }
+                if (mTextAlphabet != null && !mTextAlphabet.isEmpty()) {
+                    // create alphabet textview
+                    mImageIcon.setVisibility(GONE);
+                    mTextViewAlphabet.setVisibility(VISIBLE);
+                    mTextViewAlphabet.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTextSizeAlpha);
+                    mTextViewAlphabet.setText(mTextAlphabet);
+                }
+            } else {
+                mImageIcon.setVisibility(VISIBLE);
+                mTextViewAlphabet.setVisibility(GONE);
+                mTextViewNumeric.setVisibility(GONE);
+                // create icon
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(mDrawableSize, mDrawableSize);
+                params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+                mImageIcon.setLayoutParams(params);
+                mImageIcon.setImageDrawable(mButtonDrawable);
+            }
+
+            setTextColor(mTextColor);
+
+            a.recycle();
+        }
+
+        setClickable(true);
+        setOnClickListener(this);
+        setOrientation(HORIZONTAL);
+        LayoutParams lp = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+        lp.gravity = Gravity.CENTER;
+        setLayoutParams(lp);
+    }
+
+    /**
+     * Sets a button click listener for the button
+     * @param listener - {@link OnButtonClickListener} listener
+     */
+    public void setButtonClickListener(OnButtonClickListener listener) {
+        mButtonClickListener = listener;
+    }
+
+    public void setTextColor(ColorStateList colorStateList) {
+        if (colorStateList != null) {
+            mTextViewAlphabet.setTextColor(colorStateList);
+            mTextViewNumeric.setTextColor(colorStateList);
+        }
+    }
+
+    public void setNumericTextSize(float textSize) {
+        if (mTextViewNumeric != null) {
+            mTextSizeNumeric = textSize;
+            mTextViewNumeric.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+            requestLayout();
+        }
+    }
+
+    public void setAlphabetTextSize(float textSize) {
+        if (mTextViewAlphabet != null) {
+            mTextSizeAlpha = textSize;
+            mTextViewAlphabet.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+            requestLayout();
+        }
+    }
+
+    /**
+     * Gets the numeric text for the button
+     */
+    public String getNumericText() {
+        return mTextNumeric;
+    }
+
+    /**
+     * Sets the numeric text on the button
+     * @param text - numeric text to display on the button
+     */
+    public void setNumericText(String text) {
+        if (mTextViewNumeric != null) {
+            mTextNumeric = text;
+            mTextViewNumeric.setText(text);
+            requestLayout();
+        }
+    }
+
+    /**
+     * Gets the alphabet text for the button
+     */
+    public String getAlphabetText() {
+        return mTextAlphabet;
+    }
+
+    /**
+     * Sets the alphabet text on the button
+     * @param text - alphabet text to display on the button
+     */
+    public void setAlphabetText(String text) {
+        if (mTextViewAlphabet != null) {
+            mTextAlphabet = text;
+            mTextViewAlphabet.setText(text);
+            requestLayout();
+        }
+    }
+
+    /**
+     * @return true if the button is an image button, false otherwise
+     */
+    public boolean isImageButton() {
+        return mButtonDrawable != null;
+    }
+
+    /**
+     * Sets the image size to use for images set on the button
+     * @param imageSize - required image size in pixels
+     */
+    public void setImageIconSize(int imageSize) {
+        if (mImageIcon != null) {
+            mDrawableSize = imageSize;
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(mDrawableSize, mDrawableSize);
+            params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+            mImageIcon.setLayoutParams(params);
+            requestLayout();
+        }
+    }
+
+    /**
+     * Sets the text color to use for both the alphabet and numeric texts
+     * @param color - @{@link ColorInt} representation of the color
+     */
+    public void setTextColor(@ColorInt int color) {
+        setTextColor(ColorStateList.valueOf(color));
+        requestLayout();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (mButtonClickListener != null) {
+            mButtonClickListener.onButtonClick(this);
+        }
+    }
+}
--- pinpad/src/main/java/co/paystack/android/design/widget/PinPadView.java
+++ pinpad/src/main/java/co/paystack/android/design/widget/PinPadView.java
@@ -6,8 +6,15 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.ColorInt;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * PinPadView
@@ -16,14 +23,37 @@ public class PinPadView extends FrameLayout {
     private static final int DEFAULT_INDICATOR_COLOR = Color.WHITE;
     private static final int DEFAULT_TEXT_COLOR = Color.WHITE;
     private static final int DEFAULT_PIN_LENGTH = 4;
+    private static final float DEFAULT_TEXT_SIZE_NUMERIC = 18f;
+    private static final float DEFAULT_TEXT_SIZE_ALPHA = 12f;
+    private static final float DEFAULT_TEXT_SIZE_PROMPT = 18f;
 
     @ColorInt
     private int mIndicatorColor = DEFAULT_INDICATOR_COLOR;
 
     @ColorInt
     private int mTextColor = DEFAULT_TEXT_COLOR;
-    private String mTextPrompt;
+    private String mPromptText;
     private int mPinLength = DEFAULT_PIN_LENGTH;
+    private float mTextSizeNumeric;
+    private float mTextSizeAlpha;
+    private float mTextSizePrompt;
+    private int mDrawableSize;
+
+    private PinPadButton mButton0;
+    private PinPadButton mButton1;
+    private PinPadButton mButton2;
+    private PinPadButton mButton3;
+    private PinPadButton mButton4;
+    private PinPadButton mButton5;
+    private PinPadButton mButton6;
+    private PinPadButton mButton7;
+    private PinPadButton mButton8;
+    private PinPadButton mButton9;
+    private PinPadButton mButtonBack;
+    private PinPadButton mButtonDone;
+    private TextView mTextViewPrompt;
+
+    private List<PinPadButton> mButtons;
 
     public PinPadView(Context context) {
         super(context);
@@ -49,19 +79,149 @@ public class PinPadView extends FrameLayout {
             TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.PinPadView);
 
             mPinLength = a.getInteger(R.styleable.PinPadView_pin_length, DEFAULT_PIN_LENGTH);
+            mTextSizeNumeric = a.getDimension(R.styleable.PinPadView_button_numeric_textsize,
+                    DEFAULT_TEXT_SIZE_NUMERIC);
+            mTextSizeAlpha = a.getDimension(R.styleable.PinPadView_button_alpha_textsize,
+                    DEFAULT_TEXT_SIZE_ALPHA);
+            mTextSizePrompt = a.getDimension(R.styleable.PinPadView_prompt_textsize,
+                    DEFAULT_TEXT_SIZE_PROMPT);
+            mDrawableSize = a.getDimensionPixelSize(R.styleable.PinPadView_button_drawable_size,
+                    15);
             if (a.hasValue(R.styleable.PinPadView_pin_indicator_color)) {
                 mIndicatorColor = a.getColor(R.styleable.PinPadView_pin_indicator_color,
                         DEFAULT_INDICATOR_COLOR);
             }
 
             if (a.hasValue(R.styleable.PinPadView_text_prompt)) {
-                mTextPrompt = a.getString(R.styleable.PinPadView_text_prompt);
+                mPromptText = a.getString(R.styleable.PinPadView_text_prompt);
             }
 
-            if (a.hasValue(R.styleable.PinPadView_textColor)) {
-                mTextColor = a.getColor(R.styleable.PinPadView_textColor, DEFAULT_TEXT_COLOR);
+            if (a.hasValue(R.styleable.PinPadView_text_color)) {
+                mTextColor = a.getColor(R.styleable.PinPadView_text_color, DEFAULT_TEXT_COLOR);
             }
+
             a.recycle();
+
+            // inflate compound view;
+            View parent = inflate(context, R.layout.layout_pinpad, this);
+
+            mButton0 = (PinPadButton) parent.findViewById(R.id.ps_btn_0);
+            mButton1 = (PinPadButton) parent.findViewById(R.id.ps_btn_1);
+            mButton2 = (PinPadButton) parent.findViewById(R.id.ps_btn_2);
+            mButton3 = (PinPadButton) parent.findViewById(R.id.ps_btn_3);
+            mButton4 = (PinPadButton) parent.findViewById(R.id.ps_btn_4);
+            mButton5 = (PinPadButton) parent.findViewById(R.id.ps_btn_5);
+            mButton6 = (PinPadButton) parent.findViewById(R.id.ps_btn_6);
+            mButton7 = (PinPadButton) parent.findViewById(R.id.ps_btn_7);
+            mButton8 = (PinPadButton) parent.findViewById(R.id.ps_btn_8);
+            mButton9 = (PinPadButton) parent.findViewById(R.id.ps_btn_9);
+            mButton0 = (PinPadButton) parent.findViewById(R.id.ps_btn_0);
+            mButtonBack = (PinPadButton) parent.findViewById(R.id.ps_btn_back);
+            mTextViewPrompt = (TextView) parent.findViewById(R.id.pinpad_prompt);
+
+            mButtons = Arrays.asList(
+                    mButton0, mButton1, mButton2, mButton3, mButton4,
+                    mButton5, mButton6, mButton7, mButton8, mButton9,
+                    mButton0, mButtonBack);
+
+            //set properties;
+            setNumericTextSize(mTextSizeNumeric, false);
+            setAlphabetTextSize(mTextSizeAlpha, false);
+            setImageButtonSize(mDrawableSize, false);
+            setTextColor(mTextColor, false);
+            setPromptTextSize(mTextSizePrompt, false);
+            setPromptText(mPromptText);
+        }
+    }
+
+    /**
+     * Sets the pinpad prompt text
+     * @param promptText - text to display on the prompt field
+     */
+    public void setPromptText(String promptText) {
+        mPromptText = promptText;
+        mTextViewPrompt.setVisibility(TextUtils.isEmpty(promptText) ? GONE : VISIBLE);
+        mTextViewPrompt.setText(mPromptText);
+        requestLayout();
+    }
+
+    /**
+     * Sets the textsize for the prompt text
+     * @param textSize - textsize for the prompt text in pixels
+     */
+    public void setPromptTextSize(float textSize) {
+        setPromptTextSize(textSize, true);
+    }
+
+    private void setPromptTextSize(float textSize, boolean requestLayout) {
+        mTextViewPrompt.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        if (requestLayout) {
+            requestLayout();
+        }
+    }
+
+    public void setNumericTextSize(float textSize) {
+        setNumericTextSize(textSize, true);
+    }
+
+    private void setNumericTextSize(float textSize, boolean requestLayout) {
+        for (PinPadButton button : mButtons) {
+            if (button != null) {
+                button.setNumericTextSize(textSize);
+            }
+        }
+        if (requestLayout) {
+            requestLayout();
+        }
+    }
+
+    public void setAlphabetTextSize(float textSize) {
+        setAlphabetTextSize(textSize, true);
+    }
+
+    /**
+     * Sets the alphabet textsize with an option to request layout afterwards
+     * @param textSize - textSize for the alphabet text on the pinpad
+     * @param requestLayout - flag whether or not to call {@link #requestLayout()}
+     */
+    private void setAlphabetTextSize(float textSize, boolean requestLayout) {
+        for (PinPadButton button : mButtons) {
+            if (button != null) {
+                button.setAlphabetTextSize(textSize);
+            }
+        }
+        if (requestLayout) {
+            requestLayout();
+        }
+    }
+
+    public void setTextColor(@ColorInt int color) {
+        setTextColor(color, true);
+    }
+
+    private void setTextColor(@ColorInt int color, boolean requestLayout) {
+        for (PinPadButton button : mButtons) {
+            if (button != null) {
+                button.setTextColor(color);
+            }
+        }
+        if (requestLayout) {
+            requestLayout();
+        }
+    }
+
+    public void setImageButtonSize(int size) {
+        setImageButtonSize(size, true);
+    }
+
+    public void setImageButtonSize(int size, boolean requestLayout) {
+        for (PinPadButton button : mButtons) {
+            if (button != null && button.isImageButton()) {
+                button.setImageIconSize(size);
+            }
+        }
+        if (requestLayout) {
+            requestLayout();
         }
     }
 }
