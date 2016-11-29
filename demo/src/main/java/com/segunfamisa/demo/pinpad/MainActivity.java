c602d2e55ec70296c1089fa2438b20974f18ed50
ibrahimlawal <ibrahim@lawal.me>
2016-11-29 16:46:25 +0100

    enhance: Demo reacts visibly

--- demo/src/main/java/com/segunfamisa/demo/pinpad/MainActivity.java
+++ demo/src/main/java/com/segunfamisa/demo/pinpad/MainActivity.java
@@ -2,12 +2,39 @@ package com.segunfamisa.demo.pinpad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import co.paystack.android.design.widget.PinPadView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PinPadView pinpadView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pinpadView = (PinPadView) findViewById(R.id.pinpadView);
+        textView = (TextView) findViewById(R.id.textView);
+
+        pinpadView.setOnPinChangedListener(new PinPadView.OnPinChangedListener() {
+            @Override
+            public void onPinChanged(String oldPin, String newPin) {
+                textView.setText(textView.getText() + "\nChanged from: " + oldPin + " to " + newPin);
+            }
+
+        });
+
+        pinpadView.setOnCompletedListener(new PinPadView.OnCompletedListener() {
+            @Override
+            public void onCompleted(String pin) {
+                textView.setText(textView.getText() + "\nSubmitted: " + pin);
+
+            }
+        });
     }
+
+
 }
